1fd1d3d60a6d1cd9e5e3011127a1151082802522
Ville Vornanen <vvornanen@gmail.com>
2019-07-20 02:44:10 +0300

    Add Money

---
+++ src/money.spec.ts
@@ -0,0 +1,217 @@
+import {Currency, CurrencyError, DKK, EUR, Money, USD} from './money';
+import {expect} from 'chai';
+
+describe('Currency', () => {
+  describe('valueOf', () => {
+    it('should accept a code string', () => {
+      const currency = Currency.valueOf('EUR');
+      expect(currency).to.deep.eq(EUR);
+    });
+
+    it('should accept a custom definition', () => {
+      const currency = Currency.valueOf({code: 'XTS', exponent: 8});
+      expect(currency.code).to.eq('XTS');
+      expect(currency.exponent).to.eq(8);
+    });
+
+    it('should support different exponents with same currency code', () => {
+      const eur2 = Currency.valueOf({code: 'EUR', exponent: 2});
+      const eur3 = Currency.valueOf({code: 'EUR', exponent: 3});
+      expect(eur2.exponent).to.eq(2);
+      expect(eur3.exponent).to.eq(3);
+    });
+  });
+
+  describe('equals', () => {
+    const currency = Currency.valueOf({code: 'XTS', exponent: 2});
+
+    it('should return true if code and exponent equal', () => {
+      expect(currency.equals(Currency.valueOf({code: 'XTS', exponent: 2}))).to.eq(true);
+    });
+
+    it('should return false if code not equal', () => {
+      expect(currency.equals(Currency.valueOf({code: 'EUR', exponent: 2}))).to.eq(false);
+    });
+
+    it('should return false if exponent not equal', () => {
+      expect(currency.equals(Currency.valueOf({code: 'XTS', exponent: 3}))).to.eq(false);
+    });
+  });
+
+  describe('toString', () => {
+    it('should return the three-letter ISO 4217 code', () => {
+      const currency = Currency.valueOf('XTS');
+      expect(currency.toString()).to.eq('XTS');
+    });
+  });
+});
+
+describe('Money', () => {
+  describe('plus', () => {
+    it('should add amounts', () => {
+      const money = Money.valueOf({amount: 1, currency: EUR});
+      expect(money.plus(money).amount.toNumber()).to.eq(2);
+    });
+
+    it('should check that currencies match', () => {
+      const money1 = Money.valueOf({amount: 1, currency: EUR});
+      const money2 = Money.valueOf({amount: 1, currency: USD});
+      expect(() => money1.plus(money2)).to.throw(CurrencyError);
+    });
+  });
+
+  describe('minus', () => {
+    it('should subtract amount', () => {
+      const money = Money.valueOf({amount: 1, currency: EUR});
+      expect(money.minus(money).amount.toNumber()).to.eq(0);
+    });
+
+    it('should check that currencies match', () => {
+      const money1 = Money.valueOf({amount: 1, currency: EUR});
+      const money2 = Money.valueOf({amount: 1, currency: USD});
+      expect(() => money1.minus(money2)).to.throw(CurrencyError);
+    });
+  });
+
+  describe('mul', () => {
+    it('should multiply the amount with the given multiplier', () => {
+      const money = Money.valueOf({amount: 7, currency: EUR});
+      expect(money.mul(1.52).amount.toString()).to.eq('10.64');
+    });
+  });
+
+  describe('div', () => {
+    it('should divide the amount with the given divider', () => {
+      const money = Money.valueOf({amount: 7, currency: EUR});
+      expect(money.div(6).amount.toString()).to.eq('1.1666666666666666667');
+    });
+  });
+
+  describe('toJSON', () => {
+    it('should format all decimals', () => {
+      const money = Money.valueOf({amount: 7, currency: EUR});
+      expect(money.toJSON().amount).to.eq('7.00');
+    });
+
+    it('should format rounded amount', () => {
+      const money = Money.valueOf({amount: '1.1666666666666666667', currency: EUR});
+      expect(money.toJSON().amount).to.eq('1.17');
+    });
+
+    it('should format amount with large exponent', () => {
+      const money = Money.valueOf({amount: 15, currency: {code: 'XTS', exponent: 8}});
+      expect(money.toJSON().amount).to.eq('15.00000000');
+    });
+
+    it('should format amount with zero exponent', () => {
+      const money = Money.valueOf({amount: 120, currency: {code: 'XTS', exponent: 0}});
+      expect(money.toJSON().amount).to.eq('120');
+    });
+
+    it('should format the currency code', () => {
+      const money = Money.valueOf({amount: 4.52, currency: DKK});
+      expect(money.toJSON().currency).to.eq('DKK');
+    });
+  });
+
+  describe('allocate', () => {
+    describe('one ratio', () => {
+      const money = Money.valueOf({amount: '10.00', currency: 'EUR'});
+      const allocations = money.allocate({
+        '2019/001': 1
+      });
+
+      it('should allocate amounts according to the given ratios', () => {
+        expect(allocations['2019/001'].amount.toString()).to.eq('10');
+      });
+    });
+
+    describe('two ratios', () => {
+      const money = Money.valueOf({amount: '10.00', currency: 'EUR'});
+      const allocations = money.allocate({
+        '2019/001': 5,
+        '2019/002': 2
+      });
+
+      it('should allocate amounts according to the given ratios', () => {
+        expect(allocations['2019/001'].amount.toString()).to.eq('7.14');
+        expect(allocations['2019/002'].amount.toString()).to.eq('2.86');
+      });
+    });
+
+    describe('three ratios', () => {
+      const money = Money.valueOf({amount: '10.00', currency: 'EUR'});
+      const allocations = money.allocate({
+        '2019/001': 3,
+        '2019/002': 3,
+        '2019/003': 3
+      });
+
+      it('should allocate amounts according to the given ratios', () => {
+        expect(allocations['2019/001'].amount.toString()).to.eq('3.34');
+        expect(allocations['2019/002'].amount.toString()).to.eq('3.33');
+        expect(allocations['2019/003'].amount.toString()).to.eq('3.33');
+      });
+    });
+
+    describe('remainder distribution', () => {
+      const money = Money.valueOf({amount: '1.00', currency: 'EUR'});
+      const allocations = money.allocate({0: 1, 1: 2});
+
+      // The original Fowler's algorithm would always place the remaining cent on the first
+      // allocation (i.e. would result in [0: 0.34, 1: 0.66]), but our algorithm places it where
+      // it would go if the amounts were rounded separately
+      it('should allocate the remainder per rounding rules', () => {
+        expect(allocations[0].amount.toString()).to.eq('0.33');
+        expect(allocations[1].amount.toString()).to.eq('0.67');
+      });
+    });
+
+    describe("solving Foemmel's Conundrum", () => {
+      const money = Money.valueOf({amount: '0.05', currency: 'EUR'});
+      const allocations = money.allocate({0: 3, 1: 7});
+
+      it('should put the remainder on the first allocation', () => {
+        expect(allocations[0].amount.toString()).to.eq('0.02');
+        expect(allocations[1].amount.toString()).to.eq('0.03');
+      });
+    });
+  });
+
+  describe('convert', () => {
+    const money1 = Money.valueOf({amount: '5.20', currency: 'EUR'});
+    const money2 = money1.convertTo('USD', 1.120931);
+
+    it('should have the new currency', () => {
+      expect(money2.currency.code).to.eq('USD');
+    });
+
+    it('should be converted with the given rate', () => {
+      expect(money2.amount.toString()).to.eq('5.83');
+    });
+  });
+
+  describe('equals', () => {
+    const money = Money.valueOf({amount: 12.3, currency: EUR});
+
+    it('should return true if amount and currency equal', () => {
+      expect(money.equals(Money.valueOf({amount: 12.3, currency: EUR}))).to.eq(true);
+    });
+
+    it('should return false if amount not equal', () => {
+      expect(money.equals(Money.valueOf({amount: 5, currency: EUR}))).to.eq(false);
+    });
+
+    it('should return false if currency not equal', () => {
+      expect(money.equals(Money.valueOf({amount: 12.3, currency: USD}))).to.eq(false);
+    });
+  });
+
+  describe('toString', () => {
+    const money = Money.valueOf({amount: 12.3, currency: EUR});
+
+    it('should return a string representation', () => {
+      expect(money.toString()).to.eq('12.30 EUR');
+    });
+  });
+});
---
+++ src/money.ts
@@ -0,0 +1,251 @@
+import {Decimal} from 'decimal.js';
+
+export interface CurrencyDefinition {
+  code: string;
+  exponent: number;
+}
+
+export interface MoneyValue {
+  amount: string | number | Decimal;
+  currency: string | CurrencyDefinition;
+}
+
+export interface NumberMap {[s: string]: number}
+export interface MoneyMap {[s: string]: Money}
+
+/**
+ * An internal cache for Currency.valueOf()
+ */
+const currencies: {[s: string]: Currency} = {};
+
+/**
+ * Represents an ISO 4217 currency.
+ */
+export class Currency implements CurrencyDefinition {
+  /**
+   * A three-letter ISO 4217 code.
+   */
+  readonly code: string;
+
+  /**
+   * Number of digits after the decimal separator, varies between different currencies.
+   */
+  readonly exponent: number;
+
+  constructor(value: CurrencyDefinition) {
+    this.code = value.code;
+    this.exponent = value.exponent;
+  }
+
+  static valueOf(value: string | CurrencyDefinition): Currency {
+    if (typeof value === 'string') {
+      if (!currencies[value]) {
+        currencies[value] = new Currency({code: value, exponent: 2});
+      }
+
+      return currencies[value];
+    } else {
+      // Use a complex key just in case different exponents of the same currency were needed
+      const key = JSON.stringify(value);
+      if (!currencies[key]) {
+        currencies[key] = new Currency(value);
+      }
+
+      return currencies[key];
+    }
+  }
+
+  toString(): string {
+    return this.code;
+  }
+
+  equals(another: string | CurrencyDefinition): boolean {
+    if (typeof another === 'string') {
+      return this.code === another;
+    } else {
+      return this.code === another.code && this.exponent === another.exponent;
+    }
+  }
+}
+
+// We could define all codes here if this was a generic library,
+// but we're not going to need them just for our purposes
+export const EUR = currencies.EUR = new Currency({code: 'EUR', exponent: 2});
+export const USD = currencies.USD = new Currency({code: 'USD', exponent: 2});
+export const FIM = currencies.FIM = new Currency({code: 'FIM', exponent: 2});
+export const DKK = currencies.DKK = new Currency({code: 'DKK', exponent: 2});
+export const JPY = currencies.JPY = new Currency({code: 'JPY', exponent: 0}); // Just to demonstrate the exponent
+
+export class CurrencyError extends Error {
+  constructor(message: string) {
+    super(message);
+    this.name = 'CurrencyError';
+  }
+}
+
+/**
+ * Represents an amount of money in a specific currency.
+ *
+ * Ensures that currencies are not mixed and provides a sophisticated allocation method.
+ *
+ * Adapted from Martin Fowler's Money Pattern,
+ * see Fowler, M. (2003) Patterns of Enterprise Application Architecture, pp. 488–495.
+ *
+ * Similar to https://github.com/macor161/ts-money but with a more convenient API for our purposes.
+ * Implementation is based on decimal.js.
+ *
+ * TODO: Consider moving this code to a reusable library
+ */
+export class Money implements MoneyValue {
+  readonly amount: Decimal;
+
+  /**
+   * A three-letter ISO 4217 code.
+   */
+  readonly currency: Currency;
+
+  constructor(value: MoneyValue) {
+    if (!value.currency) {
+      throw new CurrencyError('Undefined currency');
+    }
+
+    this.amount = new Decimal(value.amount);
+    this.currency = Currency.valueOf(value.currency);
+  }
+
+  static valueOf(value: MoneyValue): Money {
+    if (value instanceof Money) {
+      return value;
+    }
+
+    return new Money(value);
+  }
+
+  plus(value: MoneyValue): Money {
+    const another = Money.valueOf(value);
+    this.checkSameCurrency(another);
+
+    return this.withAmount(this.amount.plus(another.amount));
+  }
+
+  minus(value: Money): Money {
+    const another = Money.valueOf(value);
+    this.checkSameCurrency(another);
+
+    return this.withAmount(this.amount.minus(another.amount));
+  }
+
+  mul(multiplier: string | number | Decimal): Money {
+    return this.withAmount(this.amount.mul(multiplier));
+  }
+
+  div(divider: string | number | Decimal): Money {
+    return this.withAmount(this.amount.div(divider));
+  }
+
+  convertTo(currency: string | CurrencyDefinition, rate: string | number | Decimal): Money {
+    return new Money({
+      amount: this.amount.mul(rate).toFixed(this.currency.exponent),
+      currency: currency
+    });
+  }
+
+  /**
+   * Allocates the amount by the given ratios.
+   *
+   * Guarantees that no cents are lost in the allocation.
+   *
+   * @param ratios mapped by keys that also map corresponding allocations in the result
+   */
+  allocate(ratios: NumberMap): MoneyMap {
+    if (Object.keys(ratios).length === 0) {
+      throw new Error('No ratios defined');
+    }
+
+    const sumOfRatios = Object.values(ratios).reduce((acc, ratio) => acc + ratio);
+
+    // Do the allocation with integers (i.e. minor currency units) because it's more simple
+    const multiplier = 10 ** this.currency.exponent;
+    const amountAsInt = this.amount.mul(multiplier).toNumber();
+
+    // Store a rounding result for each key
+    const remainders: {[s: string]: number} = {};
+
+    // Results contains the allocations as integers
+    const results: NumberMap = {};
+
+    let remainder = amountAsInt;
+
+    for (const [key, ratio] of Object.entries(ratios)) {
+      const result = amountAsInt * ratio / sumOfRatios;
+      results[key] = Math.floor(result);
+      remainders[key] = Math.round(result) - results[key];
+      remainder -= results[key];
+    }
+
+    // An improved version of the Fowler's algorithm. This takes rounding rules into account and
+    // distributes the remainders more evenly, not just on the first keys.
+    // It makes a difference when calculating accumulated allocations over several decades
+    // where Fowler's version would build up leftovers always on the oldest assets.
+    // e.g. allocating 1 with ratios [1, 2] should result in [0.33, 0.67], not [0.34, 0.66]
+    for (const key of Object.keys(remainders)) {
+      if (remainders[key] > 0 && remainder > 0) {
+        results[key]++;
+        remainder--;
+      }
+    }
+
+    // Allocate the remainder in case something was left over from rounding all down,
+    // e.g. allocating 1 with ratios [1, 1, 1] should result in [0.34, 0.33, 0.33]
+    for (let i = 0; i < remainder; i++) {
+      results[Object.keys(results)[i]]++;
+    }
+
+    return Object.keys(results).reduce((acc: MoneyMap, key) => {
+      acc[key] = this.withAmount(results[key] / multiplier);
+      return acc;
+    }, {});
+  }
+
+  toJSON(): MoneyValue {
+    return {
+      amount: this.amount.toFixed(this.currency.exponent),
+      currency: this.currency.toString()
+    };
+  }
+
+  toString(): string {
+    return `${this.amount.toFixed(this.currency.exponent)} ${this.currency}`;
+  }
+
+  equals(another: MoneyValue) {
+    return another.amount
+      && this.amount.equals(another.amount)
+      && this.currency.equals(another.currency);
+  }
+
+  /**
+   * Throws an error if the currency of the given object does not equal to this object.
+   *
+   * @param another
+   */
+  private checkSameCurrency(another: MoneyValue): void {
+    if (!this.currency.equals(another.currency)) {
+      throw new CurrencyError(`Expected a money object with currency ${this.currency} but got ${another.currency}`);
+    }
+  }
+
+  /**
+   * Returns a new Money object with the given amount.
+   *
+   * Retains the currency from this object.
+   *
+   * @param amount
+   */
+  private withAmount(amount: string | number | Decimal): Money {
+    return Money.valueOf({
+      amount: amount,
+      currency: this.currency
+    });
+  }
+}
